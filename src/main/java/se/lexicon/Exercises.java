df4a4a872c23ef497e32a2e728a4347fdc114551
Mohammed Dawood <m.dawood.780909@gmail.com>
2024-08-08 12:25:18 +0200

    Finished Todo 5,6

--- src/main/java/se/lexicon/Exercises.java
+++ src/main/java/se/lexicon/Exercises.java
@@ -6,12 +6,17 @@ import se.lexicon.model.Gender;
 import se.lexicon.model.Person;
 import se.lexicon.data.DataStorage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Exercises {
 
     private final static DataStorage storage = DataStorage.INSTANCE;
 
+
+    /*
+        TODO: 1. Find everyone that has firstName: “Erik” using findMany().
+     */
     public static void exercise1(String message) {
         System.out.println(message);
         // Lambda expression to find persons with firstName "Erik"
@@ -21,6 +26,10 @@ public class Exercises {
         System.out.println("----------------------");
     }
 
+
+    /*
+        TODO: 2. Find all females in the collection using findMany().
+     */
     public static void exercise2(String message) {
         System.out.println(message);
         // Lambda expression to find persons with gender FEMALE
@@ -30,6 +39,10 @@ public class Exercises {
         System.out.println("----------------------");
     }
 
+
+    /*
+       TODO: 3. Find all who are born after (and including) 2000-01-01 using findMany().
+     */
     public static void exercise3(String message) {
         System.out.println(message);
         LocalDate thresholdDate = LocalDate.of(2000, 1, 1);
@@ -42,6 +55,10 @@ public class Exercises {
         System.out.println("----------------------");
     }
 
+
+    /*
+       TODO: 4. Find the Person that has an id of 123 using findOne().
+     */
     public static void exercise4(String message) {
         System.out.println(message);
         int targetId = 123;
@@ -58,27 +75,47 @@ public class Exercises {
         System.out.println("----------------------");
     }
 
-
     /*
-        TODO:  5.	Find the Person that has an id of 456 and convert to String with following content:
-            “Name: Nisse Nilsson born 1999-09-09”. Use findOneAndMapToString().
+       TODO: 5. Find the Person that has an id of 456 and convert to String with following content:
+        “Name: Nisse Nilsson born 1999-09-09”. Use findOneAndMapToString().
      */
     public static void exercise5(String message) {
         System.out.println(message);
-        //Write your code here
+        int targetId = 456;
 
+        // Lambda expression to find the person with id 456
+        String personString = storage.findOneAndMapToString(
+                person -> person.getId() == targetId,
+                // Lambda expression to convert the person to the desired string format
+                person -> "Name: " + person.getFirstName() + " " + person.getLastName() + " born " + person.getBirthDate()
+        );
+
+        // Print the resulting string
+        System.out.println(personString);
         System.out.println("----------------------");
     }
 
+
     /*
-        TODO:  6.	Find all male people whose names start with “E” and convert each to a String using findManyAndMapEachToString().
+        TODO:  6. Find all male people whose names start with “E” and convert each to a String using findManyAndMapEachToString().
      */
     public static void exercise6(String message) {
         System.out.println(message);
-        //Write your code here
+
+        // Lambda expression to filter males whose first name starts with "E"
+        List<String> maleNamesStartingWithE = storage.findManyAndMapEachToString(
+                person ->Gender.MALE.equals(person.getGender()) && person.getFirstName().startsWith("E"),
+
+                // Lambda expression to convert each Person to a String
+                person -> person.getFirstName() + " " + person.getLastName()
+        );
+
+        // Print each resulting string
+        maleNamesStartingWithE.forEach(System.out::println);
 
         System.out.println("----------------------");
     }
+
 
     /*
         TODO:  7.	Find all people who are below age of 10 and convert them to a String like this:
